a99d6d50d65f5c456eb3c93a935c9793283a4e16
Joe Avila <josepharthuravila@gmail.com>
2020-12-20 16:18:30 -0800

    feature: add state to NewListForm

--- src/components/NewListForm.tsx
+++ src/components/NewListForm.tsx
@@ -9,15 +9,32 @@ export const NewListForm: React.FC = () => {
     const initialState = { id: 1, title: "", description: "", list: [] }
     const [list, setList] = React.useState(initialState);
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e);
+    const handleHeaderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setList({
+            ...list,
+            [e.target.id]: e.target.value
+        })
     };
 
     return (
         <Container>
             <Typography variant="h4" color="primary">Create a new Todo List.</Typography>
-            <TextField id="title" required={true} value={list.title} helperText="Name for the new list" variant="outlined" handleChange={handleChange} />
-            <TextField id="description" value={list.description} required={false} helperText="Describe the list." variant="outlined" handleChange={handleChange} />
+            <TextField
+                id="title"
+                required={true}
+                value={list.title}
+                helperText="Name for the new list"
+                variant="outlined"
+                handleChange={handleHeaderChange}
+            />
+            <TextField
+                id="description"
+                value={list.description}
+                required={false}
+                helperText="Describe the list."
+                variant="outlined"
+                handleChange={handleHeaderChange}
+            />
 
 
         </Container>
